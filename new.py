ff483027c74456563680930072aaa9cb5e2f0a03
Rapeti Veda Priya <vedapr471@gmail.com>
2025-04-13 17:35:55 +0530

    Update new.py

--- new.py
+++ new.py
@@ -18,9 +18,10 @@ if platform.system().lower() == 'windows':
 def capture_screenshot(username):
     options = Options()
     options.binary_location = "/usr/bin/chromium"
-    options.add_argument("--headless")
+    options.add_argument("--headless=new")
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")
 
     if platform.system() != 'Windows':
